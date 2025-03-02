b54a6f2c4165721289cd43cc78767d11682a0d4a
Harepriya12 <111402855+Harepriya12@users.noreply.github.com>
2025-03-02 11:41:55 +0800

    Add files via upload
    week1

---
+++ day1/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("The developer name is:"+" " +developer);
+		System.out.println("The version is:"+" " +version);
+		System.out.println("The release year is:"+" " +releaseYear);
+		System.out.println("The shortcut key is:"+" " +shortcutKey);
+		System.out.println("whether is it beta:"+" " +isBeta);
+		
+
+	}
+
+}
